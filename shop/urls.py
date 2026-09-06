80df63dcda42f8e5836f97d9c6ffcc14d88fad27
Nikhitha Damarla <damarlanikhitha@gmail.com>
2026-09-06 12:44:51 +0530

    Standardize my orders URL

--- shop/urls.py
+++ shop/urls.py
@@ -69,7 +69,7 @@ path(
 ),
     path("payment/", views.payment, name="payment"),
     path("order-success/", views.order_success, name="order_success"),
-    path("my-orders", views.my_orders, name="my_orders"),
+    path("my-orders/", views.my_orders, name="my_orders"),
     path(
     "cancel-order/<int:order_id>/",
     views.cancel_order,
